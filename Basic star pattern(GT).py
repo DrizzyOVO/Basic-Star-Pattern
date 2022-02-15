185627b136f6c36b05ef5bd2c043ad0dbdc9d814
Gaurav Malpedi <gaurav.malpedi17@gmail.com>
2022-02-15 19:43:46 +0530

    Initial commit

---
+++ Basic star pattern(GT).py
@@ -0,0 +1,11 @@
+# num = int(input("Which number do you wanna see star pattern in\n "
+#                 "note :- your decided number will serve as the number of rows\n"))
+# v = int(input("Enter 1 for ascending order and 0 as descending order\n"))
+# if v == 1:
+#     for i in range(0, num+1):
+#         print("*" * i)
+# elif v == 0:
+#     for i in range(num, 0, -1):
+#         print("*" * i)
+# else:
+#     print("Enter numbers according to the information provided for the desired pattern")
